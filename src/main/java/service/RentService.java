8a43cbf5ee4815543257c907a9f149ccecf70719
Leonardo Tadeu <this.leotsl@gmail.com>
2021-03-04 16:20:33 -0300

    Developing new functionalities using TDD.

--- src/main/java/service/RentService.java
+++ src/main/java/service/RentService.java
@@ -6,7 +6,9 @@ import entity.User;
 import exceptions.InventoryException;
 import exceptions.MovieException;
 import exceptions.UserException;
+import utils.DateUtils;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
@@ -29,13 +31,33 @@ public class RentService {
             }
         }
 
+        double percentageDiscount = applyDiscount(movies);
+
         Rent rent = new Rent();
         rent.setMovies(movies);
         rent.setUser(user);
         rent.setDateRent(new Date());
         rent.setPrice(0d);
         movies.forEach(movie -> rent.setPrice(rent.getPrice() + movie.getRentPrice()));
+        rent.setPrice(rent.getPrice() + (rent.getPrice() * percentageDiscount));
         rent.setDateReturn(getDateWithDifferenceOfDays(1));
+
+        if (DateUtils.isDayOfWeek(rent.getDateReturn(), Calendar.MONDAY)) {
+            rent.setDateReturn(getDateWithDifferenceOfDays(2));
+        }
+
         return rent;
+    }
+
+    public double applyDiscount(List<Movie> movies) {
+        double percentage = 0;
+        if (movies.size() >= 3) {
+            int countMovies = movies.size() - 2;
+            while (countMovies > 0 && percentage < 1.0) {
+                percentage += 0.25;
+                countMovies--;
+            }
+        }
+        return percentage;
     }
 }
--- src/test/java/service/RentServiceTest.java
+++ src/test/java/service/RentServiceTest.java
@@ -1,21 +1,20 @@
 package service;
 
 import entity.Movie;
+import entity.Rent;
 import entity.User;
 import exceptions.InventoryException;
 import exceptions.MovieException;
 import exceptions.UserException;
 import exceptions.VideoStoreException;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.rules.ErrorCollector;
 import org.junit.rules.ExpectedException;
+import utils.DateUtils;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.*;
 
 public class RentServiceTest {
 
@@ -34,12 +33,9 @@ public class RentServiceTest {
 
     @Test(expected = UserException.class)
     public void rentUserNull() throws VideoStoreException {
-        List<Movie> movies = new ArrayList<>();
-        Movie movie1 = new Movie("Movie 1", 3, 10d);
-        Movie movie2 = new Movie("Movie 2", 2, 8d);
-        movies.add(movie1);
-        movies.add(movie2);
-
+        List<Movie> movies = new ArrayList<>(Arrays
+                .asList(new Movie("Movie 1", 3, 10d),
+                        new Movie("Movie 2", 2, 8d)));
         service.rentMovies(null, movies);
     }
 
@@ -52,26 +48,122 @@ public class RentServiceTest {
 
     @Test(expected = InventoryException.class)
     public void rentInventoryIsEmpty() throws VideoStoreException {
-        List<Movie> movies = new ArrayList<>();
-        Movie movie1 = new Movie("Movie 1", 3, 10d);
-        Movie movie2 = new Movie("Movie 2", 0, 8d);
-        movies.add(movie1);
-        movies.add(movie2);
-
+        List<Movie> movies = new ArrayList<>(Arrays
+                .asList(new Movie("Movie 1", 3, 10d),
+                        new Movie("Movie 2", 0, 8d)));
         User user = new User("User 1");
         service.rentMovies(user, movies);
     }
 
     @Test
     public void rentIsNotNull() throws VideoStoreException {
-        List<Movie> movies = new ArrayList<>();
-        Movie movie1 = new Movie("Movie 1", 3, 10d);
-        Movie movie2 = new Movie("Movie 2", 2, 8d);
-        movies.add(movie1);
-        movies.add(movie2);
-
+        List<Movie> movies = new ArrayList<>(Arrays
+                .asList(new Movie("Movie 1", 3, 10d),
+                        new Movie("Movie 2", 2, 8d)));
         User user = new User("User 1");
 
         error.checkThat(service.rentMovies(user, movies), notNullValue());
+    }
+
+
+    @Test
+    public void rentApplyDiscount() throws VideoStoreException {
+        List<Movie> movies = new ArrayList<>(Arrays
+                .asList(new Movie("Movie 1", 3, 10d),
+                        new Movie("Movie 2", 2, 8d),
+                        new Movie("Movie 3", 5, 7d),
+                        new Movie("Movie 4", 2, 8d),
+                        new Movie("Movie 5", 5, 5d)));
+        User user = new User("User 1");
+
+        Rent rent = service.rentMovies(user, movies);
+
+        error.checkThat(rent.getPrice(), is(66.5));
+    }
+
+    @Test
+    public void getProgressiveDiscount25() {
+        List<Movie> movies = new ArrayList<>(Arrays
+                .asList(new Movie("Movie 1", 3, 10d),
+                        new Movie("Movie 2", 2, 8d),
+                        new Movie("Movie 3", 5, 7d)));
+
+        double finalPrice = service.applyDiscount(movies);
+
+        error.checkThat(finalPrice, equalTo(0.25));
+    }
+
+    @Test
+    public void getProgressiveDiscount50() {
+        List<Movie> movies = new ArrayList<>(Arrays
+                .asList(new Movie("Movie 1", 3, 10d),
+                        new Movie("Movie 2", 2, 8d),
+                        new Movie("Movie 3", 5, 7d),
+                        new Movie("Movie 4", 2, 8d)));
+
+        double finalPrice = service.applyDiscount(movies);
+
+        error.checkThat(finalPrice, equalTo(0.50));
+    }
+
+    @Test
+    public void getProgressiveDiscount75() {
+        List<Movie> movies = new ArrayList<>(Arrays
+                .asList(new Movie("Movie 1", 3, 10d),
+                        new Movie("Movie 2", 2, 8d),
+                        new Movie("Movie 3", 5, 7d),
+                        new Movie("Movie 4", 2, 8d),
+                        new Movie("Movie 5", 1, 15d)));
+
+        double finalPrice = service.applyDiscount(movies);
+
+        error.checkThat(finalPrice, equalTo(0.75));
+    }
+
+    @Test
+    public void getProgressiveDiscount100() {
+        List<Movie> movies = new ArrayList<>(Arrays
+                .asList(new Movie("Movie 1", 3, 10d),
+                        new Movie("Movie 2", 2, 8d),
+                        new Movie("Movie 3", 5, 7d),
+                        new Movie("Movie 4", 2, 8d),
+                        new Movie("Movie 5", 1, 15d),
+                        new Movie("Movie 6", 5, 5d)));
+
+        double finalPrice = service.applyDiscount(movies);
+
+        error.checkThat(finalPrice, equalTo(1.0));
+    }
+
+    @Test
+    public void getProgressiveDiscountMoreMovies() {
+        List<Movie> movies = new ArrayList<>(Arrays
+                .asList(new Movie("Movie 1", 3, 10d),
+                        new Movie("Movie 2", 2, 8d),
+                        new Movie("Movie 3", 5, 7d),
+                        new Movie("Movie 4", 2, 8d),
+                        new Movie("Movie 5", 1, 15d),
+                        new Movie("Movie 6", 5, 5d),
+                        new Movie("Movie 7", 5, 5d)));
+
+        double finalPrice = service.applyDiscount(movies);
+
+        error.checkThat(finalPrice, equalTo(1.0));
+    }
+
+    @Test
+    @Ignore
+    public void notReturnMoviesOnSaturday() throws MovieException, InventoryException, UserException {
+        Assume.assumeTrue(DateUtils.isDayOfWeek(new Date(), Calendar.SATURDAY));
+
+        List<Movie> movies = new ArrayList<>(Arrays
+                .asList(new Movie("Movie 1", 3, 10d),
+                        new Movie("Movie 2", 2, 8d)));
+        User user = new User("User 1");
+
+        Rent rent = service.rentMovies(user, movies);
+        boolean isSunday = DateUtils.isDayOfWeek(rent.getDateReturn(), Calendar.SUNDAY);
+
+        Assert.assertTrue(isSunday);
     }
 }
